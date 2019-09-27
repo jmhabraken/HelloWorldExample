36b545bd4f443c39710a1ba5c36243ba244d1910
jmhabraken <habrakenjm@gmail.com>
2019-09-27 17:16:13 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String voornaam;
+
+        voornaam = "Job";
+
+        System.out.println("Hallo "+voornaam);
+    }
+}
